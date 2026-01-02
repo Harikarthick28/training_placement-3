928695c3972d11f7063a7c0156d57f6bbbd2007e
Harikarthick <147905893+Harikarthick28@users.noreply.github.com>
2026-01-02 13:09:33 +0530

    Create 2darray.java

---
+++ 22.12.2025/2darray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class twodarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //int r = sc.nextInt();
+        //int c= sc.nextInt();
+        int[][] arr = new int[3][3];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int sum=0;
+        for(int i=0;i<3;i++){
+            sum=sum+(arr[i][3-i-1]);
+                //System.out.print(arr[i][j]+" ");
+            
+        }
+        System.out.println(sum);
+
+    }
+    
+}
