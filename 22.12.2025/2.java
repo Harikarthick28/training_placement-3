0eca16603853b310bbb701e5548e0963a2b9b2ec
Harikarthick <147905893+Harikarthick28@users.noreply.github.com>
2026-01-02 13:08:41 +0530

    Create 2.java

---
+++ 22.12.2025/2.java
@@ -0,0 +1,16 @@
+
+class twosum{
+    public static void main(String[] args) {
+        int[] arr={1,2,4,5};
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+        
+        System.out.print(arr[i]);
+        }
+        System.out.println("");
+    }
+    }
+
+}
+
+
