4caaadbe9f139dcff252a1c85cd333bece7d9213
Harikarthick <147905893+Harikarthick28@users.noreply.github.com>
2026-01-02 12:59:49 +0530

    Create 2.java

---
+++ 20.12.2025/2.java
@@ -0,0 +1,15 @@
+public class rshift {
+    public static void main(String[] args) {
+        byte a = 101;
+        short b = 10000;
+        long c= 1000000000;
+        int d =93983456;
+        float e = 1.23456f;
+        double f = 1.234567890123456d;
+        char g = 'a';
+        boolean h = true;
+        System.out.println(a);
+
+    }
+    
+}
