61e2bfc184a301d8ff27df901edcf12b96baee67
Harikarthick <147905893+Harikarthick28@users.noreply.github.com>
2026-01-18 20:09:40 +0530

    Create 2.java

---
+++ 16.1.2026/2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char firstNonRepeating(String s) {
+        int[] freq = new int[256];
+
+        for (char c : s.toCharArray())
+            freq[c]++;
+
+        for (char c : s.toCharArray()) {
+            if (freq[c] == 1)
+                return c;
+        }
+        return '_';
+    }
+}
